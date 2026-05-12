b8c7eae453a72096e5ef296f21edcb4c85ce32b5
SaymaShinha <45699081+SaymaShinha@users.noreply.github.com>
2026-05-12 09:11:11 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(cors());
 
 app.use(cors({
   origin: ["https://glowify-cosmetics.netlify.app",
-           "https://glowify-eight.vercel.app"]
+           "https://glowify-eight.vercel.app"],
   credentials: true
 }));
 
